e75c32f92ea5fb8b234f8ed76d8e0d269a9b4a81
kateyatsk <klaettoe2023@gmail.com>
2025-03-05 12:28:06 +0300

    Initial Commit

---
+++ LayoutAccountScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutAccountScreen
+//
+//  Created by Екатерина Яцкевич on 5.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
